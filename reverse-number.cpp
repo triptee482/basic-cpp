de633310a5ebe16c856d9cc69ea1a264fcdb1148
triptee482 <triptiyadav881@gmail.com>
2024-12-29 19:06:02 +0530

    added reverse number program

--- reverse-number.cpp
+++ reverse-number.cpp
@@ -1,5 +1,24 @@
 #include <iostream>
 
-void main(){
-    
+int main()
+{
+    int n;
+
+    std::cout<<"Enter a number: ";
+    std::cin>>n;
+    std::cout<<"You entered: "<<n<<std::endl;
+
+    int reverse = 0;
+
+    while (n != 0)
+    {
+        reverse = (reverse + n % 10) * 10;
+        n /= 10;
+    }
+
+    reverse /= 10;
+
+    std::cout << "reverse: " << reverse;
+
+    return 0;
 }
